5ca2348667037c58cbb82a39863bafe8d68d1c80
Carson274 <carsonsecrest@gmail.com>
2024-02-27 23:14:00 -0800

    Switched to a different U-Net

--- unet.py
+++ unet.py
@@ -1,121 +1,46 @@
-import torch
-import torch.nn as nn
-from torchvision import models
-from torch.nn.functional import relu
-import torch.nn.functional as F
+from unet_parts import *
+
 
 class UNet(nn.Module):
-  def __init__(self, n_class):
-    super().__init__()
-    
-    # Encoder
-    # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
-    # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
-    # -------
-    # input: 572x572x1 - 1 channel for grayscale
-    self.e11 = nn.Conv2d(1, 64, kernel_size=3, padding=1) # output: 570x570x64
-    self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
-    self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
 
-    # input: 284x284x64
-    self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
-    self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
-    self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
+        self.inc = (DoubleConv(n_channels, 64))
+        self.down1 = (Down(64, 128))
+        self.down2 = (Down(128, 256))
+        self.down3 = (Down(256, 512))
+        factor = 2 if bilinear else 1
+        self.down4 = (Down(512, 1024 // factor))
+        self.up1 = (Up(1024, 512 // factor, bilinear))
+        self.up2 = (Up(512, 256 // factor, bilinear))
+        self.up3 = (Up(256, 128 // factor, bilinear))
+        self.up4 = (Up(128, 64, bilinear))
+        self.outc = (OutConv(64, n_classes))
 
-    # input: 140x140x128
-    self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
-    self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
-    self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
 
-    # input: 68x68x256
-    self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
-    self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
-    self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
-
-    # input: 32x32x512
-    self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
-    self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
-
-
-    # Decoder
-    self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-    self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-    self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-
-    self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-    self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-    self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-
-    self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-    self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-    self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-
-    self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-    self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-    self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-    # Output layer
-    self.outconv = nn.Conv2d(64, n_class, kernel_size=1)
-
-  def forward(self, x):
-    # Encoder
-    xe11 = relu(self.e11(x))
-    xe12 = relu(self.e12(xe11))
-    xp1 = self.pool1(xe12)
-
-    xe21 = relu(self.e21(xp1))
-    xe22 = relu(self.e22(xe21))
-    xp2 = self.pool2(xe22)
-
-    xe31 = relu(self.e31(xp2))
-    xe32 = relu(self.e32(xe31))
-    xp3 = self.pool3(xe32)
-
-    xe41 = relu(self.e41(xp3))
-    xe42 = relu(self.e42(xe41))
-    xp4 = self.pool4(xe42)
-
-    xe51 = relu(self.e51(xp4))
-    xe52 = relu(self.e52(xe51))
-    
-    # decoder
-    xu1 = self.upconv1(xe52)
-    diffY = xe42.size()[2] - xu1.size()[2]
-    diffX = xe42.size()[3] - xu1.size()[3]
-    xu1 = F.pad(xu1, [diffX // 2, diffX - diffX // 2,
-                      diffY // 2, diffY - diffY // 2])
-    xu11 = torch.cat([xu1, xe42], dim=1)
-    xd11 = relu(self.d11(xu11))
-    xd12 = relu(self.d12(xd11))
-
-    xu2 = self.upconv2(xd12)
-    diffY = xe32.size()[2] - xu2.size()[2]
-    diffX = xe32.size()[3] - xu2.size()[3]
-    xu2 = F.pad(xu2, [diffX // 2, diffX - diffX // 2,
-                      diffY // 2, diffY - diffY // 2])
-    xu22 = torch.cat([xu2, xe32], dim=1)
-    xd21 = relu(self.d21(xu22))
-    xd22 = relu(self.d22(xd21))
-
-    xu3 = self.upconv3(xd22)
-    diffY = xe22.size()[2] - xu3.size()[2]
-    diffX = xe22.size()[3] - xu3.size()[3]
-    xu3 = F.pad(xu3, [diffX // 2, diffX - diffX // 2,
-                      diffY // 2, diffY - diffY // 2])
-    xu33 = torch.cat([xu3, xe22], dim=1)
-    xd31 = relu(self.d31(xu33))
-    xd32 = relu(self.d32(xd31))
-
-    xu4 = self.upconv4(xd32)
-    diffY = xe12.size()[2] - xu4.size()[2]
-    diffX = xe12.size()[3] - xu4.size()[3]
-    xu4 = F.pad(xu4, [diffX // 2, diffX - diffX // 2,
-                      diffY // 2, diffY - diffY // 2])
-    xu44 = torch.cat([xu4, xe12], dim=1)
-    xd41 = relu(self.d41(xu44))
-    xd42 = relu(self.d42(xd41))
-
-    # Output layer
-    out = self.outconv(xd42)
-
-    return out
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.down3 = torch.utils.checkpoint(self.down3)
+        self.down4 = torch.utils.checkpoint(self.down4)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.up3 = torch.utils.checkpoint(self.up3)
+        self.up4 = torch.utils.checkpoint(self.up4)
+        self.outc = torch.utils.checkpoint(self.outc)
